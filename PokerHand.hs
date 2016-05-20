c35aebb6d14c78e0a25dc669e11605560bc346de
ToF- <cthibaut@octo.com>
2016-05-20 16:25:48 +0200

    pair relative to highcard

--- PokerHand.hs
+++ PokerHand.hs
@@ -1,21 +1,26 @@
 module PokerHand
 where
-import Data.List (sort)
+import Data.List (sort, group)
 import Data.Ord (comparing)
 
 type Card = Int
+
+data Category = HighCard | Pair
+    deriving (Eq, Ord, Show)
 
 data Hand = Hand [Card]
     deriving (Eq, Show)
 
 instance Ord Hand
     where
-    compare = comparing (reverse.sort.cards)
-data Category = HighCard
-    deriving (Eq, Ord, Show)
+    compare h g = case comparing category h g of 
+                                  EQ -> comparing (reverse.sort.cards) h g
+                                  r  -> r
 
 category :: Hand -> Category
-category _ = HighCard
+category h = case length (group (cards h)) of
+                4 -> Pair
+                _ -> HighCard
 
 cards :: Hand -> [Card]
 cards (Hand cs) = cs
--- Tests.hs
+++ Tests.hs
@@ -6,13 +6,20 @@ rsort = reverse . sort
 
 instance Arbitrary Hand
     where
-    arbitrary = vectorOf 5 arbitrary >>= (return.Hand)
+    arbitrary = frequency
+                [(50, vectorOf 5 arbitrary >>= (return.Hand))
+                ,(50, vectorOf 4 arbitrary >>= \cs -> return (Hand (head cs:cs)))]
 
 highCardRelativeOrder h g = (category h == HighCard
                           && category g == HighCard)
                      ==> (h > g) ==>
     rsort (cards h) > rsort (cards g) 
 
-main = quickCheck highCardRelativeOrder
+pairToHighCardOrder h g = (category h == Pair
+                        && category g == HighCard)
+                     ==> h > g
+main = do 
+    quickCheck highCardRelativeOrder
+    quickCheck pairToHighCardOrder
 
 
